82b89aa62803e673aab13369aea48d889e7a204a
N <boc228sk@gmail.com>
2026-06-16 20:47:07 +0900

    Add blood type fortune messages to main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+if (bloodType === "A型") {
+    console.log("大吉")
+} else if (bloodType === "B型") {
+    console.log("中吉")
+} else if (bloodType === "O型") {
+    console.log("凶")
+} else if (bloodType === "AB型") {
+    console.log("大吉")
+}
